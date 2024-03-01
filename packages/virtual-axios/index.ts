4c5627518ce812f54fddbb46855aea1fe0e7ab81
yechao <1151062047@qq.com>
2024-03-01 15:06:37 +0800

    feat: 配置自定义拦截器

--- packages/virtual-axios/index.ts
+++ packages/virtual-axios/index.ts
@@ -1,4 +1,4 @@
-import { BaseConfig } from "./type";
+import { BaseConfig, Interceptor } from "./type";
 
 
 class virtualAxios {
@@ -7,6 +7,35 @@ class virtualAxios {
   constructor(axios: any, config: BaseConfig) {
     this.instance = axios;
     this.config = config
+  }
+
+  //配置自定义拦截器
+  setCustomInterceptors() {
+    const interceptors = this.config?.interceptors || []
+    if (!interceptors.length) return;
+    for (let interceptor of interceptors) {
+      this.generateInterceptor(interceptor)
+    }
+  }
+
+  //拦截器生成函数
+  generateInterceptor(interceptor: Interceptor) {
+    this.instance.interceptor[interceptor.type].use((config: any) => {
+      const url = interceptor.type === 'request' ? config.url : config.config.url
+      if (interceptor.coverAll) config.success(config)
+      else if (interceptor.scopePorts?.includes(url)) config.success(config)
+      return config
+    }, (error: any) => {
+      if (interceptor.coverAll) error.success(error)
+      else if (interceptor.scopePorts?.includes(error.config.url)) error.success(error)
+      return Promise.reject(error)
+     })
+  }
+
+  //配置重试
+  setRetry() {
+    if (!this.config.retry?.enable) return;
+    
   }
 }
 
